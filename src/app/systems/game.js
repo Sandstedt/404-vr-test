cfe36124071a40b3ef411267db0614f8a52d1969
Jonas Sandstedt <jonas.sandstedt@fullystudios.se>
2020-08-15 17:35:49 +0200

    Added a basic interaction

--- src/app/systems/game.js
+++ src/app/systems/game.js
@@ -1,25 +1,4 @@
 // import {Box} from "../entities/Box";
-
-AFRAME.registerComponent('change-color-on-hover', {
-  schema: {
-    color: {default: 'red'}
-  },
-
-  init: function () {
-    var data = this.data;
-    var el = this.el;  // <a-box>
-    var defaultColor = el.getAttribute('material').color;
-
-    el.addEventListener('mouseenter', function () {
-    el.setAttribute('color', data.color);
-    });
-
-    el.addEventListener('mouseleave', function () {
-    el.setAttribute('color', defaultColor);
-    });
-  }
-});
-
 AFRAME.registerSystem('game', {
   schema: {},
 
