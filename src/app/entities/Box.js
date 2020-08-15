75d3b630c3d6ff842a518953facaf1cd62f57cc9
Jonas Sandstedt <jonas.sandstedt@fullystudios.se>
2020-08-15 16:05:02 +0200

    Small screen, need some more space

--- src/app/entities/Box.js
+++ src/app/entities/Box.js
@@ -1,34 +1,34 @@
 export class Box {
-    constructor(x, y, z, settings) {
-        this.scene = document.querySelector('a-scene');
-        this.el = document.createElement('a-entity');
+  constructor(x, y, z, settings) {
+    this.scene = document.querySelector('a-scene');
+    this.el = document.createElement('a-entity');
 
-        this.el.setAttribute('position', {
-            x: x || 0,
-            y: y || 0,
-            z: z || 0
-        });
-        this.el.setAttribute('geometry', {
-            primitive: 'box',
-            width: settings.width || 1,
-            height: settings.height || 1,
-            depth: settings.depth || 1
-        });
-        this.el.setAttribute('material', {
-            opacity: 1,
-            shader: 'standard'
-        });
+    this.el.setAttribute('position', {
+      x: x || 0,
+      y: y || 0,
+      z: z || 0
+    });
+    this.el.setAttribute('geometry', {
+      primitive: 'box',
+      width: settings.width || 1,
+      height: settings.height || 1,
+      depth: settings.depth || 1
+    });
+    this.el.setAttribute('material', {
+      opacity: 1,
+      shader: 'standard'
+    });
 
-        this.el.setAttribute('visible', true);
+    this.el.setAttribute('visible', true);
 
-        this.el.className = 'box';
+    this.el.className = 'box';
 
-        this.scene.appendChild(this.el);
-    }
+    this.scene.appendChild(this.el);
+  }
 
-    update(time, timeDelta) {
-        this.el.object3D.rotation.y += 0.001 * timeDelta;
-        // var rotation = this.el.getAttribute('rotation');
-        // this.el.setAttribute('rotation', { y: rotation.y + 0.02 * timeDelta });
-    }
+  update(time, timeDelta) {
+    this.el.object3D.rotation.y += 0.001 * timeDelta;
+    // var rotation = this.el.getAttribute('rotation');
+    // this.el.setAttribute('rotation', { y: rotation.y + 0.02 * timeDelta });
+  }
 }
--- src/app/systems/game.js
+++ src/app/systems/game.js
@@ -1,21 +1,41 @@
 // import {Box} from "../entities/Box";
 
+AFRAME.registerComponent('change-color-on-hover', {
+  schema: {
+    color: {default: 'red'}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;  // <a-box>
+    var defaultColor = el.getAttribute('material').color;
+
+    el.addEventListener('mouseenter', function () {
+    el.setAttribute('color', data.color);
+    });
+
+    el.addEventListener('mouseleave', function () {
+    el.setAttribute('color', defaultColor);
+    });
+  }
+});
+
 AFRAME.registerSystem('game', {
-    schema: {},
+  schema: {},
 
-    init: function () {
-        console.log('Game Initialized');
+  init: function () {
+    console.log('Game Initialized');
 
-        // // Example summon a custom entity
-        // this.box = new Box(0, 2, -5, {
-        //    width: 1,
-        //    height: 1,
-        //    depth: 1
-        // });
-    },
+    // // Example summon a custom entity
+    // this.box = new Box(0, 2, -5, {
+    //  width: 1,
+    //  height: 1,
+    //  depth: 1
+    // });
+  },
 
-    tick(time, timeDelta){
-        // Your gameloop code
-        // this.box.update(time, timeDelta);
-    }
+  tick(time, timeDelta){
+    // Your gameloop code
+    // this.box.update(time, timeDelta);
+  }
 });
